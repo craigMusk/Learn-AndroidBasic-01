27e6adaa22a3e73769aa66273b7f470c74c311f9
CraigMusk <32991669+craigMusk@users.noreply.github.com>
2017-10-21 19:50:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sam/notekeeper/AñoInfo.java
@@ -0,0 +1,17 @@
+package com.example.sam.notekeeper;
+
+/**
+ * Created by Sam on 21/10/2017.
+ */
+
+public class AñoInfo {
+
+    private String nombre;
+    private String año;
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
